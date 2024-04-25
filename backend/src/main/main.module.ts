1ca1000316db1e7b1613966c83f76ae934cf0e63
Lenny <lenny.donnez@gmail.com>
2024-04-25 12:13:33 +0200

    Add health module to main

--- backend/src/main/main.module.ts
+++ backend/src/main/main.module.ts
@@ -2,8 +2,9 @@ import { AuthModule } from "src/auth/auth.module"
 import { Module } from "@nestjs/common"
 import { AppModule } from "src/app/app.module"
 import { PrismaModule } from "src/prisma/prisma.module"
+import { HealthModule } from "src/health/health.module"
 
 @Module({
-  imports: [PrismaModule, AuthModule, AppModule],
+  imports: [PrismaModule, AuthModule, AppModule, HealthModule],
 })
 export class MainModule {}
