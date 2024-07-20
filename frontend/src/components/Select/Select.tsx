aeb495cfda0b15474586dec3a7a4704bbf3dc7ef
Lenny <lenny.donnez@gmail.com>
2024-07-20 12:27:47 +0200

    refactor for headless ui v2

--- frontend/src/components/Select/Select.tsx
+++ frontend/src/components/Select/Select.tsx
@@ -1,7 +1,5 @@
-import { Fragment } from "react"
-import { Listbox, Transition } from "@headlessui/react"
+import { Label, Listbox, ListboxButton, ListboxOption, ListboxOptions } from "@headlessui/react"
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid"
-import classNames from "classnames"
 
 type Option = {
   key: string
@@ -19,71 +17,45 @@ type Props<T extends Option> = {
 export const Select = <T extends Option>({ label, options, value, onChange }: Props<T>) => {
   return (
     <Listbox value={value} onChange={onChange}>
-      {({ open }) => (
-        <>
-          <Listbox.Label className="block leading-6">{label}</Listbox.Label>
-          <div className="relative mt-1">
-            <Listbox.Button
-              data-testid="select-button"
-              className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6"
-            >
-              <span className="flex items-center">
-                <span className="ml-3 block truncate">{value.label}</span>
-              </span>
-              <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-              </span>
-            </Listbox.Button>
+      <Label className="block leading-6">{label}</Label>
+      <div className="relative mt-1">
+        <ListboxButton
+          data-testid="select-button"
+          className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+        >
+          <span className="flex items-center">
+            <span className="ml-3 block truncate">{value.label}</span>
+          </span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+            <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </span>
+        </ListboxButton>
 
-            <Transition
-              show={open}
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
+        <ListboxOptions
+          transition
+          className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+        >
+          {options.map((option) => (
+            <ListboxOption
+              key={option.key}
+              data-testid={`${option.value}-option`}
+              className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+              value={option}
             >
-              <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {options.map((option) => (
-                  <Listbox.Option
-                    key={option.key}
-                    data-testid={`${option.value}-option`}
-                    className={({ active }) =>
-                      classNames(
-                        active ? "bg-indigo-600 text-white" : "text-gray-900",
-                        "relative cursor-default select-none py-2 pl-3 pr-9",
-                      )
-                    }
-                    value={option}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex items-center">
-                          <span
-                            className={classNames(selected ? "font-semibold" : "font-normal", "ml-3 block truncate")}
-                          >
-                            {option.label}
-                          </span>
-                        </div>
-
-                        {selected ? (
-                          <span
-                            className={classNames(
-                              active ? "text-white" : "text-indigo-600",
-                              "absolute inset-y-0 right-0 flex items-center pr-4",
-                            )}
-                          >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </>
-      )}
+              <>
+                <div className="flex items-center">
+                  <span className="ml-3 block truncate font-normal group-data-[selected]:font-semibold">
+                    {option.label}
+                  </span>
+                </div>
+                <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+                  <CheckIcon aria-hidden="true" className="h-5 w-5" />
+                </span>
+              </>
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </div>
     </Listbox>
   )
 }
