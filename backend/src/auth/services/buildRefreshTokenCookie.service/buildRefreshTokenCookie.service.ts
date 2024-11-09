38143326bc23ecc9cd6050326153a7d6c01575ab
Lenny <lenny.donnez@gmail.com>
2024-11-09 16:34:29 +0100

    Implement cleaner way to build cookies

--- backend/src/auth/services/buildRefreshTokenCookie.service/buildRefreshTokenCookie.service.ts
+++ backend/src/auth/services/buildRefreshTokenCookie.service/buildRefreshTokenCookie.service.ts
@@ -6,11 +6,24 @@ export class BuildRefreshTokenCookieService {
   constructor(private readonly configService: ConfigService) {}
 
   public execute(refreshToken: string): string {
+    const cookieSettings = this.buildCookieSettings()
+    const name = this.configService.get("HTTP_COOKIE_REFRESH_TOKEN_NAME")
+    return `${name}=${refreshToken}; ${cookieSettings}`
+  }
+
+  private buildCookieSettings() {
     const cookieExpireTime = this.configService.get("HTTP_COOKIE_EXPIRE_TIME")
     const cookieDomain = this.configService.get("HTTP_COOKIE_DOMAIN")
     const cookieSameSite = this.configService.get("HTTP_COOKIE_SAME_SITE")
-    const name = this.configService.get("HTTP_COOKIE_REFRESH_TOKEN_NAME")
 
-    return `${name}=${refreshToken}; HttpOnly; Secure; domain=${cookieDomain}; Path=/; SameSite=${cookieSameSite}; Max-Age=${cookieExpireTime}`
+    const cookieSettings = [
+      `domain=${cookieDomain}`,
+      `HttpOnly`,
+      `Path=/`,
+      `SameSite=${cookieSameSite}`,
+      `Max-Age=${cookieExpireTime}`,
+    ]
+
+    return cookieSettings.join("; ")
   }
 }
