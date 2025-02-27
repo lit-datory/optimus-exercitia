6ad1ebb07760f2d3a50adccbb893a44869c481f3
Lenny <lenny.donnez@gmail.com>
2025-02-27 18:06:12 +0100

    Ensure get profile after log in

--- frontend/src/features/auth/store/auth.atom.ts
+++ frontend/src/features/auth/store/auth.atom.ts
@@ -1,12 +1,14 @@
 import { atom } from "jotai"
 import { login, logout } from "../api"
 import { LoginFieldValues } from "../types"
+import { profileAtom } from "src/features/profile/store"
 
 export const accessTokenAtom = atom<string | undefined>(undefined)
 
 export const loginAtom = atom(null, async (_, set, data: LoginFieldValues) => {
   const { accessToken } = await login(data)
   set(accessTokenAtom, accessToken)
+  await set(profileAtom)
 })
 
 export const logoutAtom = atom(null, async (_, set) => {
